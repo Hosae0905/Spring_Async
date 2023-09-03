0e5ad02367d7c26bd44a9ba0557c789cabc90250
didgh <didghtls11@gmail.com>
2023-09-03 21:48:59 +0900

    스프링 비동기 공부

---
+++ src/main/java/dev/be/async/sevice/EmailService.java
@@ -0,0 +1,18 @@
+package dev.be.async.sevice;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    @Async("defaultTaskExecutor")
+    public void sendMail() {
+        System.out.println("{sendMail} :: " + Thread.currentThread().getName());
+    }
+    @Async("messagingTaskExecutor")
+    public void sendMailWithCustomThreadPool() {
+        System.out.println("{messagingTaskExecutor} :: " + Thread.currentThread().getName());
+    }
+}
